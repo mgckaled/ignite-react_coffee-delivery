407c3d668e803fddef2a69ac14523113075d6d2c
mgckaled <marcelgckaled@gmail.com>
2023-02-24 17:37:25 -0300

    styles: add global and default styles by ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
 	return (
-		<>
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyles />
 			<h1>Desafio Ignite</h1>
-		</>
+		</ThemeProvider>
 	)
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+		scroll-behavior: smooth;
+  }
+
+	body, input, textarea, button {
+		font-family: 'Roboto', sans-serif;
+		font-weight: 400;
+		font-size: 1rem;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	body {
+		background: ${(props) => props.theme.background};
+	}
+
+  :focus {
+    outline: 0;
+  }
+
+
+  button {
+    cursor: pointer;
+  }
+
+	 a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  input {
+    outline: none;
+  }
+
+	// Remove o icone (seta) do input tipo númerico //
+	/* Chrome, Safari, Edge, Opera */
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+	-webkit-appearance: none;
+	margin: 0;
+	}
+	
+	/* Firefox */
+	input[type=number] {
+	-moz-appearance: textfield;
+	}
+	
+`
