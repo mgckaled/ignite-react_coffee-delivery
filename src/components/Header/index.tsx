d2e45eaef2ef15dba49d7d1378bc5ba426308dfa
mgckaled <marcelgckaled@gmail.com>
2023-02-24 19:05:51 -0300

    feat: add styles, logo, button and icons to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,34 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+
+import { Button } from '../../components/Button'
+
+import { HeaderContainer, HeaderContent } from './styles'
+
+import coffeeDeliveryLogo from '../../assets/logo.svg'
+
 export function Header() {
 	return (
-		<header>
-			<h1>Header</h1>
-		</header>
+		<HeaderContainer>
+			<img
+				src={coffeeDeliveryLogo}
+				alt="Imagem de um copo de café roxo com um simbolo de um foguete e ao lado escrito Coffee Delivery"
+				width={84}
+				height={40}
+			/>
+
+			<HeaderContent>
+				<Button color="purple-light">
+					<MapPin size={22} weight="fill" className="header__map-pin_icon" />
+					<span>Juiz de Fora, MG</span>
+				</Button>
+				<Button color="yellow-light">
+					<ShoppingCart
+						size={22}
+						weight="fill"
+						className="header__shopping-cart_icon"
+					/>
+				</Button>
+			</HeaderContent>
+		</HeaderContainer>
 	)
 }
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+
 	max-width: 80rem;
 	margin: 0 auto;
-	/* padding: 2.5rem; */
+	padding: 2rem 0;
+
 	background: ${(props) => props.theme.background};
 `
