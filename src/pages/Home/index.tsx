675a54300a347c33d2bf8223e9840dea30fe978f
mgckaled <marcelgckaled@gmail.com>
2023-02-25 09:59:42 -0300

    feat(Home page): insert text banner and svg image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,38 @@
+import coffeeDeliveryImg from '../../assets/coffeeDelivery.svg'
+
+import {
+	Banner,
+	BannerDescription,
+	BannerLeft,
+	BannerRight,
+	BannerTitle,
+	HomeContainer,
+} from './styles'
+
 export function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<HomeContainer>
+			<Banner>
+				<BannerRight>
+					<div>
+						<BannerTitle>
+							Encontre o café perfeito <br />
+							para qualquer hora do dia.
+						</BannerTitle>
+						<BannerDescription>
+							Com o Coffee Delivery você recebe seu café onde estiver, a <br />
+							qualquer hora
+						</BannerDescription>
+					</div>
+				</BannerRight>
+				<BannerLeft>
+					<img
+						src={coffeeDeliveryImg}
+						alt="Um copo de Café escrito Coffee Delivery"
+						title="Coffee Delivery"
+					/>
+				</BannerLeft>
+			</Banner>
+		</HomeContainer>
 	)
 }
